19800991375400f219c816b42e44850373e3ff89
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-05-21 00:49:32 -0500

    [update Repository]

--- SprinSecurityApp/src/main/java/com/app/persistence/repository/UserRepository.java
+++ SprinSecurityApp/src/main/java/com/app/persistence/repository/UserRepository.java
@@ -1,10 +1,20 @@
 package com.app.persistence.repository;
 
 import com.app.persistence.entities.UserEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    //Nuestro querymethod
+    Optional<UserEntity> findUserEntityByUsername(String username);
+
+    /*SENTENCIA CREADA CON EL QUERY
+    @Query("SELECT u FROM UserEntity u WHERE u.username = ?")
+    Optional <UserEntity> findUser(String username);*/
+
 
 }
