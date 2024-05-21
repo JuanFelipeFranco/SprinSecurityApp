99e1fc736805c1ccb8a7fa1b191f524f36509514
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-05-20 21:43:56 -0500

    [Created  User Repository - CrudReposiory]

---
+++ SprinSecurityApp/src/main/java/com/app/persistence/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.app.persistence.repository;
+
+import com.app.persistence.entities.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+}
